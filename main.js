a7fff85c27e6118927b7947ffb4f079c0701cd0b
Eleonora Losciuto <eleonora.losciuto@gmail.com>
2026-07-19 19:21:20 +0200

    add button to generate new list

--- main.js
+++ main.js
@@ -1,29 +1,41 @@
-const arrResult = [];
 const tableEmail = document.querySelector('div.email-list');
+const emailBtn = document.getElementById('emailGenerateBtn')
 
-for (let i = 0; i < 10; i++) {
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(response => {
-            // Aggiungo l'email all'array
-            arrResult.push(response.data.response);
-            
-            // CONTROLLO: Se l'array ha 10 elementi, tutte le chiamate sono finite
-            if (arrResult.length === 10) {
-                renderEmail(arrResult)
-            }       
-        })
-        .catch(error => {
-            console.error("Errore durante la chiamata:", error);
-        })
+const generateAPIEmail = () => {
+
+    const arrResult = [];
+
+    tableEmail.innerHTML = 'Caricamento in corso...'
+
+    for (let i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+                // Aggiungo l'email all'array
+                arrResult.push(response.data.response);
+
+                // CONTROLLO: Se l'array ha 10 elementi, tutte le chiamate sono finite
+                if (arrResult.length === 10) {
+                    renderEmail(arrResult)
+                }
+            })
+            .catch(error => {
+                console.error("Errore durante la chiamata:", error);
+            })
+    }
 };
+
+//chiamata alla funzione al caricamento della pagina
+generateAPIEmail();
 
 // Aggiungo lista a pagina HTML
 const renderEmail = (arr) => {
     let html = ""
-    for (let i = 0; i < arr.length; i++) {    
+    for (let i = 0; i < arr.length; i++) {
         html += `<p>  
-        ${i+1}: ${arr[i]}
+        ${i + 1}: ${arr[i]}
         </p>`
     }
-    tableEmail.innerHTML=html;
+    tableEmail.innerHTML = html;
 };
+
+emailBtn.addEventListener('click', generateAPIEmail);
